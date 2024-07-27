a44cadc935a7642f3aed06fcb957235d9c4a0920
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-07-27 14:21:42 +0800

    change to vue

--- web/src/api/client.ts
+++
@@ -1,7 +0,0 @@
-import ky from "ky";
-
-export const api = ky.create({ prefixUrl: import.meta.env.VITE_BFF_URL });
-export enum ApiPath {
-  CalendarEvents = "calendar-events",
-  Auth = "auth",
-}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,7 @@
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [vue()],
+})
